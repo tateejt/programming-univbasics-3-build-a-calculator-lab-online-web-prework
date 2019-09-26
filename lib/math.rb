4df707c698388423d4e8349b3fa9b039e8a6eff8
congenial-compressor-7742 <tateejt@gmail.com>
2019-09-26 03:34:54 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,34 @@
 def addition(num1, num2)
-
+  num1 + num2
 end
 
 def subtraction(num1, num2)
+  num1 - num2
 
 end
 
 def division(num1, num2)
+  num1 / num2
 
 end
 
 def multiplication(num1, num2)
+  num1 * num2
 
 end
 
 def modulo(num1, num2)
+  num1 % num2
 
 end
 
 def square_root(num)
+  Math.sqrt(num)
 
 end
+
+addition(5,4)
+subtraction(10,5)
+multiplication(4,30)
+modulo(34,5)
+square_root(9)
